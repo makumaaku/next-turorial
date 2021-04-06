5fea704b5ae52f1da20960eb6b292ab009dd5c5e
MarkSaito <abokadoaoinaaiueo37@gmail.com>
2021-04-06 23:09:42 +0900

    make basic blog card

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -3,7 +3,9 @@ import Layout, { siteTitle } from '../../components/layout'
 import utilStyles from '../../styles/utils.module.css'
 import { getSortedPostsData } from '../../lib/posts'
 import Link from 'next/link'
+import Image from 'next/image'
 import Date from '../../components/date'
+import BlogCard from '../../components/organizms/blog-card'
 import { GetStaticProps } from 'next'
 
 export default function Home({
@@ -12,6 +14,7 @@ export default function Home({
   allPostsData: {
     date: string
     title: string
+    imageUrl: string
     id: string
   }[]
 }) {
@@ -23,15 +26,9 @@ export default function Home({
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
+          {allPostsData.map(({ id, date, title, imageUrl }) => (
             <li className={utilStyles.listItem} key={id}>
-              <Link href={`/posts/${id}`}>
-                <a>{title}</a>
-              </Link>
-              <br />
-              <small className={utilStyles.lightText}>
-                <Date dateString={date} />
-              </small>
+              <BlogCard imageUrl={imageUrl} pass={`/posts/${id}`} title={title} dateString={date} />
             </li>
           ))}
         </ul>
