3c1b73e7c9c323dbc381abc08081ec286dc54d80
MarkSaito <abokadoaoinaaiueo37@gmail.com>
2021-04-29 10:16:56 +0900

    add phrase book and use network image

--- next.config.js
+++ next.config.js
@@ -2,4 +2,7 @@ module.exports = {
   env: {
     STATIC_FORMS_ACCESS_KEY: process.env.STATIC_FORMS_ACCESS_KEY,
   },
+  images: {
+    domains: ['is5-ssl.mzstatic.com','is4-ssl.mzstatic.com','is1-ssl.mzstatic.com','is2-ssl.mzstatic.com'],
+},
 };
