b62f3d90fd2aa39bcdecfb634ba793ce80b7384c
MarkSaito <abokadoaoinaaiueo37@gmail.com>
2022-03-04 00:58:07 +0900

    build通るところまで

--- src/lib/posts.ts
+++ src/lib/posts.ts
@@ -1,9 +1,9 @@
 import fs from "fs";
 import path from "path";
 import matter from "gray-matter";
-import remark from "remark";
+import { remark } from 'remark'
+import html from 'remark-html'
 import highlight from 'remark-highlight.js'
-import html from "remark-html";
 
 const postsDirectory = path.join(process.cwd(), "posts");
 
