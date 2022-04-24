7690c562ccfa3622825b81c0fcef22c95e0f3c0d
MarkSaito <abokadoaoinaaiueo37@gmail.com>
2022-04-24 17:55:14 +0900

    googleアナリティクスの修正

--- src/lib/gtag.tsx
+++ src/lib/gtag.tsx
@@ -62,14 +62,18 @@ export function GoogleAnalytics() {
             src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
             strategy="afterInteractive"
           />
-          <Script id="ga" defer strategy="afterInteractive">
-            {`
-                    window.dataLayer = window.dataLayer || [];
-                    function gtag(){dataLayer.push(arguments);}
-                    gtag('js', new Date());    
-                    gtag('config', '${GA_ID}');
-                `}
-          </Script>
+          {/* Scriptを使うと、gtagがundefinedになってしまう.. */}
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+                  gtag('config', '${GA_ID}', {
+                    page_path: window.location.pathname,
+                  });`,
+            }}
+          />
         </>
       )}
     </>
