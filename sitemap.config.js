e373d51b27bfecb6c86728063976dfdcdbe195c2
MarkSaito <abokadoaoinaaiueo37@gmail.com>
2022-05-28 15:41:45 +0900

    yarn build & yarn postbuild でサイトマップが自動生成できるように

---
+++ sitemap.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    siteUrl:'https://www.m-firstapp.com/',
+    generateRobotsTxt: true, // (optional)
+    sitemapSize: 7000,
+    outDir: './out',
+    // ...other options
+  }
