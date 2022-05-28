9ae487b45140e7a60857d1bc9510d1f8ff19e823
MarkSaito <abokadoaoinaaiueo37@gmail.com>
2022-05-28 16:11:05 +0900

    サイトマップ修正

--- sitemap.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    siteUrl:'https://www.m-firstapp.com/',
-    generateRobotsTxt: true, // (optional)
-    sitemapSize: 7000,
-    outDir: './out',
-    // ...other options
-  }
